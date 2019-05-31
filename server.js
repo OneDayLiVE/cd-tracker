236ff40adef801ce61774f49e1705030a382f957
Bradmkjr <brad@ucanbonline.net>
2019-05-31 10:12:49 -0600

    allow cross domain

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var ObjectID = mongodb.ObjectID;
 
 var CONTACTS_COLLECTION = "contacts";
 
+var VISITORS_COLLECTION = "visitors";
+
 var app = express();
 app.use(bodyParser.json());
 
@@ -12,6 +14,7 @@ app.use(bodyParser.json());
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
+app.use(allowCrossDomain);
 // Rest of server.js code below
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
@@ -114,3 +117,80 @@ app.delete("/api/contacts/:id", function(req, res) {
     }
   });
 });
+
+
+////////
+
+/*  "/api/visitors"
+ *    GET: finds all visitors
+ *    POST: creates a new visitor
+ */
+
+app.get("/api/visitors", function(req, res) {
+  db.collection(VISITORS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get visitors.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
+app.post("/api/visitors", function(req, res) {
+  var newVisitor = req.body;
+  newVisitor.createDate = new Date();
+
+  if (!req.body.name) {
+    handleError(res, "Invalid user input", "Must provide a name.", 400);
+  } else {
+    db.collection(VISITORS_COLLECTION).insertOne(newVisitor, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to create new visitor.");
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
+  }
+});
+
+/*  "/api/visitors/:id"
+ *    GET: find visitor by id
+ *    PUT: update visitor by id
+ *    DELETE: deletes visitor by id
+ */
+
+app.get("/api/visitors/:id", function(req, res) {
+  db.collection(VISITORS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get visitor");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
+
+app.put("/api/visitors/:id", function(req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
+
+  db.collection(VISITORS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to update visitor");
+    } else {
+      updateDoc._id = req.params.id;
+      res.status(200).json(updateDoc);
+    }
+  });
+});
+
+app.delete("/api/visitors/:id", function(req, res) {
+  db.collection(VISITORS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete visitor");
+    } else {
+      res.status(200).json(req.params.id);
+    }
+  });
+});
+
+
